2b51b891f1a05a1a3e2c2cc17c286eb51f452f91
JeremyDevCode <jeremydevcode@gmail.com>
2023-03-17 11:01:45 -0500

    Copy to clipboard

--- src/components/GifsSection.jsx
+++ src/components/GifsSection.jsx
@@ -1,10 +1,25 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import ThunderIcon from "../assets/icons/ThunderIcon";
 import { TrendingIcon } from "../assets/icons/TrendingIcon";
-import { useGifty } from "../modules/useGifty";
-import style from "../styles/GifsLayout.module.css";
 
 function GifsSection({ gifs, isSearching }) {
+  const [check, setCheck] = useState(false);
+
+  function copyImage(source) {
+    navigator.clipboard
+      .writeText(source)
+      .then(() => {
+        setCheck(true);
+        setTimeout(() => {
+          setCheck(false);
+        }, 1500);
+      })
+      .catch((e) => {
+        alert("Cannot copy password to clipboard");
+        console.log(e);
+      });
+  }
+
   return (
     <main className="flex flex-col m-0 w-full justify-center gap-5">
       <div className="flex items-center gap-2">
@@ -16,6 +31,7 @@ function GifsSection({ gifs, isSearching }) {
           if (gif?.images.downsized.url) {
             return (
               <img
+                onClick={() => copyImage(gif?.images?.downsized?.url)}
                 className="w-full rounded-md object-cover mb-5"
                 key={gif?.id}
                 src={gif?.images?.downsized?.url}
