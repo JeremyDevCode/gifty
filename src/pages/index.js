a14c573382ed82c6d5530de3c9d56d9a44c22e7f
JeremyDevCode <jeremydevcode@gmail.com>
2023-03-19 14:50:42 -0500

    Adding head

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,12 +4,20 @@ import { Search } from "../components/Search";
 import { GifsSection } from "../components/GifsSection";
 import { useGifty } from "../modules/useGifty";
 import { Footer } from "../components/Footer";
+import Head from "next/head";
 
 export default function Home() {
   const { state } = useGifty();
   const { trending } = state;
   return (
     <div className="w-screen bg-bgBlack h-fit min-h-screen text-primaryBText">
+      <Head>
+        <title>{"Gifty"}</title>
+        <meta
+          name="description"
+          content="Web application to find funny gifs and share them with your friends"
+        ></meta>
+      </Head>
       <div className="sm:px-[10%] px-5">
         <Navbar />
         <Header />
