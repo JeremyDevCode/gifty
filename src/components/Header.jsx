71b12fd0292f5360f74826adf0267d69deef170c
JeremyDevCode <jeremydevcode@gmail.com>
2023-03-19 15:05:53 -0500

    :construction_worker: Fixing responsive design

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,7 +4,7 @@ import style from "../styles/Header.module.css";
 function Header() {
   return (
     <header className="flex items-center justify-center">
-      <h1 className="flex gap-5 sm:gap-0 sm:text-[80px] text-[1.5rem] font-black text-center py-10 sm:py-5 tracking-[-0.02em] select-none">
+      <h1 className="flex gap-5 sm:gap-0 sm:h-[180px] sm:text-[36px] lg:text-[80px] lg:h-fit text-[1.5rem] font-black text-center py-10 sm:py-5 tracking-[-0.02em] select-none">
         <span className={style.item}>Search.</span>
         <span className={style.item}>Enjoy.</span>
         <span className={style.item}>Share.</span>
