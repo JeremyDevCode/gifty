c050373f002f26c1b3cf1f1edb6b5cb99442a763
JeremyDevCode <jeremydevcode@gmail.com>
2023-03-20 18:06:49 -0500

    Restructuring gifsSection

---
+++ src/components/Gif.jsx
@@ -0,0 +1,57 @@
+import React, { useState } from "react";
+import { CopiedButton } from "./CopiedButton";
+import { CopyButton } from "./CopyButton";
+
+function Gif({ gifs }) {
+  const [check, setCheck] = useState(false);
+
+  function copyImage(source) {
+    navigator.clipboard
+      .writeText(source)
+      .then(() => {
+        setCheck(true);
+        setTimeout(() => {
+          setCheck(false);
+        }, 1500);
+      })
+      .catch((e) => {
+        alert("Cannot copy gif to clipboard");
+        console.log(e);
+      });
+  }
+
+  return (
+    <>
+      {gifs?.map((gif) => {
+        if (gif?.images.downsized.url) {
+          return (
+            <div
+              key={gif.id}
+              className="flex items-center justify-center relative w-full mb-5 group/details"
+            >
+              <img
+                onClick={() => copyImage(gif?.images?.downsized?.url)}
+                className="w-full rounded-md object-cover"
+                src={gif?.images?.downsized?.url}
+                alt={gif?.title}
+              />
+
+              <div className="hidden absolute w-full h-full bg-[#0A0A0A80] group-hover/details:flex">
+                {!check && (
+                  <CopyButton
+                    handleClick={() => copyImage(gif?.images?.downsized?.url)}
+                  />
+                )}
+                {check && <CopiedButton />}
+              </div>
+            </div>
+          );
+        } else {
+          return null;
+        }
+      })}
+    </>
+  );
+}
+
+export { Gif };
--- src/components/GifsSection.jsx
+++ src/components/GifsSection.jsx
@@ -1,28 +1,10 @@
-import React, { useEffect, useRef, useState } from "react";
+import React from "react";
 import ThunderIcon from "../assets/icons/ThunderIcon";
 import { TrendingIcon } from "../assets/icons/TrendingIcon";
-import { CopiedButton } from "./CopiedButton";
-import { CopyButton } from "./CopyButton";
+import { Gif } from "./Gif";
 import { Loading } from "./Loading";
 
 function GifsSection({ gifs, loading, isSearching }) {
-  const [check, setCheck] = useState(false);
-
-  function copyImage(source) {
-    navigator.clipboard
-      .writeText(source)
-      .then(() => {
-        setCheck(true);
-        setTimeout(() => {
-          setCheck(false);
-        }, 1500);
-      })
-      .catch((e) => {
-        alert("Cannot copy password to clipboard");
-        console.log(e);
-      });
-  }
-
   return (
     <main className="flex flex-col m-0 w-full justify-center gap-5">
       <div className="flex items-center gap-2">
@@ -31,33 +13,7 @@ function GifsSection({ gifs, loading, isSearching }) {
       </div>
       {loading && <Loading />}
       <div className="columns-2 sm:columns-3 lg:columns-4 gap-5">
-        {gifs?.map((gif) => {
-          if (gif?.images.downsized.url) {
-            return (
-              <div
-                key={gif.id}
-                className="flex items-center justify-center relative w-full mb-5 group/details"
-              >
-                <img
-                  onClick={() => copyImage(gif?.images?.downsized?.url)}
-                  className="w-full rounded-md object-cover"
-                  src={gif?.images?.downsized?.url}
-                  alt={gif?.title}
-                />
-                <div className="hidden absolute w-full h-full bg-[#0A0A0A80] group-hover/details:flex">
-                  {!check && (
-                    <CopyButton
-                      handleClick={() => copyImage(gif?.images?.downsized?.url)}
-                    />
-                  )}
-                  {check && <CopiedButton />}
-                </div>
-              </div>
-            );
-          } else {
-            return null;
-          }
-        })}
+        <Gif gifs={gifs} />
       </div>
     </main>
   );
